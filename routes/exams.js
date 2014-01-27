b5bcf09ad1e73f0e45254379819d510c9aecd8cf
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-01-26 23:06:53 -0330

    Routes return promises

--- routes/exams.js
+++ routes/exams.js
@@ -1,10 +1,13 @@
 "use strict";
 
+var q = require("q");
+
 var route = module.exports = {};
 
 route.path = "/exams";
 route.handlers = {
 	"get": function get(request, response) {
-		response.write("GET exams");
+		response.write("GET exams\n");
+		return q(true);
 	}
 };
