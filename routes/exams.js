ccd0f357558bc2d69ad5b920fb40bb1242bdaacf
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-02-16 11:35:45 -0330

    Send WWW-Authenticate header when unauthorized

--- routes/exams.js
+++ routes/exams.js
@@ -7,11 +7,14 @@ var json = {
 		"get":
 			function get(request, response) {
 				return request.authentication()
+				.fail(function (error) {
+					response.setHeader("WWW-Authenticate", "Basic realm=\"Self Service\"");
+					throw error;
+				})
 				.then(MUN.selfService.finalExamSchedule)
 				.then(function (result) {
 					var stdout = result[0];
 					response.write(stdout);
-					return true;
 				});
 			}
 	}
