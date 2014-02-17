f8c666cdf38034cb5f142cb07b1db55ee80e630b
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-02-16 22:51:48 -0330

    Use HTTPS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var port = process.env.PORT || 8080;
+var file = require("fs").readFileSync;
 var glob = require("glob");
 var path = require("path");
 
@@ -10,7 +11,12 @@ var routes = glob.sync("routes/*.js").map(function (e, i, a) {
 	return require(path.join(__dirname, e));
 });
 
-var server = rested.createServer({ "routes": routes });
+var server = rested.createServer({
+	"crt": file(path.join(__dirname, "keys/api.munapps.ca.crt")),
+	"key": file(path.join(__dirname, "keys/api.munapps.ca.key")),
+	"secure": true,
+	"routes": routes
+});
 
 server.listen(port, function () {
 	console.log("Server listening on port " + port);
