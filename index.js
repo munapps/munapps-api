9a6e44cf555aa7541a6936727b9d091bf09106ac
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-02-21 23:56:00 -0330

    Headers common to all requests

--- index.js
+++ index.js
@@ -15,13 +15,17 @@ var server = rested.createServer({
 	"crt": file(path.join(__dirname, "files/api.munapps.ca.crt")),
 	"key": file(path.join(__dirname, "files/api.munapps.ca.key")),
 	"secure": true,
-	"routes": routes
-});
-
-server.eachRequest(function (request, response) {
-	response.setHeader("X-Powered-By", "Coffee");
-	response.setHeader("X-Shenanigans", "none");
-	response.setHeader("X-GitHub", "http://git.io/3K55mA");
+	"routes": routes,
+	"headers": [{
+		"k": "X-Powered-By",
+		"v": "Coffee"
+	}, {
+		"k": "X-Shenanigans",
+		"v": "none"
+	}, {
+		"k": "X-GitHub",
+		"v": "http://git.io/3K55mA"
+	}]
 });
 
 server.listen(port, function () {
